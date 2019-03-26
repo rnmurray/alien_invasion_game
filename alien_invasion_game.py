268486cd796c7b94567a3a78bcb01da4ee3516fe
rnmurray <the.blue.phoenix1024@gmail.com>
2019-03-26 08:43:41 -0700

    Added files March 26
    Added code files and notes to repository

---
+++ alien_invasion_game.py
@@ -0,0 +1,37 @@
+# alien_invasion_game.py
+# Robyn Murray      3.25.19
+# Version 1
+
+
+#######################################################################
+# import modules
+import sys
+
+import pygame
+
+from settings import Settings
+
+# create a function to run the game
+def run_game():
+    # init game, settings, and create screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    
+    # start main While loop for the game
+    while True:
+        
+        # watch for keyboard/mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # redraw the screen during each pass through loop
+        screen.fill(ai_settings.bg_color)
+        
+        # make most recent screen visible for smooth movement
+        pygame.display.flip()
+        
+# call the run game function
+run_game()
---
+++ settings.py
@@ -0,0 +1,13 @@
+# settings.py
+# Robyn Murray      3.25.19
+# Version 1
+
+# creates a settings class for modifying game settings in Alien invastion
+class Settings():
+    
+    #initialize function for game's settings
+    def __init__(self):
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
